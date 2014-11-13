bf44213beaed43e6da2ea059ea8e53ff9d0eed8e
socretes <andysharp2003@Yahoo.com>
2014-11-13 14:53:10 +0000

    another push

---
+++ AngularServiceStackMongo/AngularServiceStackMongo.ServiceModel/Hello.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularServiceStackMongo.ServiceModel
+{
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+}
---
+++ AngularServiceStackMongo/AngularServiceStackMongo.Tests/Domain/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿namespace Domain.Test
+{
+    using System;
+    using System.Linq;
+    using AngularServiceStackMongo.Domain;
+    using ServiceStack.Testing;
+    using ServiceStack;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SendRequest()
+        {
+            var client = new JsonServiceClient(@"http://localhost:49581/api/Items");
+
+            var response = client.Post(new AngularServiceStackMongo.ServiceModel.Item { Name = "World!" });
+        }
+
+        [TestMethod]
+        public void AddItemsToMongo()
+        {
+            var item = new Item("item1", "andy", DateTime.Now, new SubItem[] { new SubItem("item1", "andy", DateTime.Now) });
+            var service = new ItemRepository();
+            service.SaveUpdate(item);
+
+            var item2 = service.GetItemDetails(20, 0);
+
+            var item3 = item2.First();
+            item3.UserName = "Fin";
+
+            service.Update(item3);
+        }
+    }
+}
